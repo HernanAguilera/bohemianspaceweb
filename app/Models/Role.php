d581e2f4bae57ec638d1b803294c0bba9cfa8f50
Hernan Aguilera <haguilera@protonmail.com>
2022-03-06 22:52:44 -0600

    permissions model created

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -12,4 +12,8 @@ class Role extends Model
     public function users() {
         return $this->belongsToMany(User::class, 'role_user');
     }
+
+    public function permissions() {
+        return $this->belongsToMany(Permission::class, 'permission_role');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
     public function roles() {
         return $this->belongsToMany(Role::class, 'role_user');
     }
+
+    public function permissions() {
+        return $this->belongsToMany(Permission::class, 'permission_user');
+    }
 }
